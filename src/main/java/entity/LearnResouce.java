570dabeb9be7a8e8107e6c04cf0e3ef355b9ac56
yangjiahao <yangjiahao@leedarson.com>
2020-04-20 08:45:06 +0800

    2020-04-20

---
+++ src/main/java/entity/LearnResouce.java
@@ -0,0 +1,11 @@
+package entity;
+
+import lombok.Data;
+
+@Data
+public class LearnResouce {
+    private Long id;
+    private String author;
+    private String title;
+    private String url;
+}
