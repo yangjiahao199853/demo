ad75f5d9077b08446a5159997abe8ba1214c717b
yangjiahao <yangjiahao@leedarson.com>
2020-04-20 17:58:00 +0800

    2020-04-20

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
